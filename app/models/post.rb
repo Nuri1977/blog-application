46603117323260221bdeb65613e0035b33e211d9
Nuri1977 <nurilacka@gmail.com>
2022-04-29 17:25:52 +0200

    Process data to models, add methods for updating counters for posts, comments and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,23 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  def update_posts_counter
+    users = User.all
+    users.each do |user|
+      if(user.id == self.author_id)
+        user.update_column(:posts_counter, user.posts.count)
+      end
+    end
+  end
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).take(5)
+  end
+
 end
