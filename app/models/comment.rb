98927ce34c035c56af095b38ce404ff4ab40aec3
Nuri1977 <nurilacka@gmail.com>
2022-05-03 10:25:37 +0200

    Implement Rails Active Callbacks after_save for comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  private
+
   def increment_comments_counter
     post.increment!(:comments_counter)
   end
