2fa01822c185e2a46f965a18600797574bf8573b
Nuri1977 <nurilacka@gmail.com>
2022-05-03 10:19:02 +0200

    Change 'take' method with 'limit' to  return an ActiveRecord Relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
 
   def recent_posts
-    posts.order(created_at: :desc).take(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
