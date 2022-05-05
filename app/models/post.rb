791a53dcc76b143e1fcfa4ec1237a76363ab5526
Nuri1977 <nurilacka@gmail.com>
2022-05-05 11:24:58 +0200

    Add validations for User and Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, presence: true,
+                                               numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
