9ecab69ece63d338560f285fbd9b2becef62626e
Nuri1977 <nurilacka@gmail.com>
2022-05-05 12:03:52 +0200

    Fix specs for responses for Users and Posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
     before(:each) do
-      get '/users/0/posts'
+      user = User.create(name: 'Nuri', bio: 'biography for testing', photo: 'photo link')
+      get user_posts_path(user_id: user.id)
     end
 
     it 'GET requests response status was correct' do
@@ -15,12 +16,14 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'response body includes correct placeholder text' do
-      expect(response.body).to include('Welocme to posts page')
+      expect(response.body).to include('List of all posts')
     end
 
     describe 'GET #show' do
       before(:each) do
-        get '/users/1/posts/1'
+        user = User.create(name: 'Nuri', bio: 'biography for testing', photo: 'photo link')
+        post = Post.create(author: user, title: 'My title', text: 'Some text')
+        get user_post_path(user_id: user.id, id: post.id)
       end
 
       it 'GET requests response status was correct' do
@@ -32,7 +35,7 @@ RSpec.describe 'Posts', type: :request do
       end
 
       it 'renders content correctly' do
-        expect(response.body).to include('Welcome to show page')
+        expect(response.body).to include('Welcome to post show page')
       end
     end
   end
