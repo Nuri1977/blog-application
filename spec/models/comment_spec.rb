e8b95750e12c3b84761e0f9c8a1b06fdc9aa2f5a
Nuri1977 <nurilacka@gmail.com>
2022-05-05 14:08:44 +0200

    Add specs for model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations For the Comment model' do
+    before(:each) do
+      @user = User.new(name: 'Nuri', photo: 'image_link', bio: 'Developer from Macedonia')
+      @post = Post.new(author: @user, title: 'My title', text: 'My text')
+      @comment = Comment.new(text: 'First comment', author_id: @user.id, post_id: @post.id)
+    end
+
+    it 'if title is present' do
+      @comment.text = nil
+      expect(@comment).to_not be_valid
+    end
+
+    it 'if author_id is integer' do
+      @comment.author_id = 'string'
+      expect(@comment).to_not be_valid
+    end
+
+    it 'if post_id is integer' do
+      @comment.post_id = 'string'
+      expect(@comment).to_not be_valid
+    end
+  end
 end
