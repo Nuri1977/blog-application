e80cd4ee3a7da9d8c656297e33f30f5353e88cea
Nuri1977 <nurilacka@gmail.com>
2022-05-10 15:41:50 +0200

    Add more specs for login view

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -20,4 +20,25 @@ RSpec.describe 'Login page', type: :feature do
     click_button 'Log in'
     expect(page).to have_content 'Welcome to Users page'
   end
+
+  it 'without filling in the username and the password, I get a detailed error' do
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+
+  it 'with incorrect data, I get a detailed error' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'testsdbsb@email.com'
+    fill_in 'Password', with: 'passwordsdbsfbsb'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+
+  it 'with correct data, I am redirected to the root page' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'test@email.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    expect(current_path).to eq root_path
+  end
 end
