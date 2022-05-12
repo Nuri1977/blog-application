a10c839e72eea2e7fca9f1e20ed1850128927e84
Nuri1977 <nurilacka@gmail.com>
2022-05-12 12:32:56 +0200

    Add registration action in api authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,13 @@
 class AuthenticationController < ApplicationController
   protect_from_forgery with: :null_session
-  skip_before_action :authenticate_request, only: [:login]
-  # POST /auth/login
+  skip_before_action :authenticate_request, only: %i[login register]
+
+  def register
+    @user = User.create! name: params[:name], email: params[:email], password: params[:password], bio: params[:bio],
+                         photo: params[:photo], confirmed_at: Time.now
+    render json: { user: @user }, status: :ok
+  end
+
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
